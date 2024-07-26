d96948bdcfe20f59e23e78a758ec335e95495419
Shivambhardwaj13 <shivam.bhardwaj13@outlook.com>
2024-07-26 18:13:53 +0530

    first commit

---
+++ src/Main.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class Main {
+
+
+
+		private static String checkPrime ( int n){
+			for (int i = 2; i <= n / 2; i++) {
+				if (n % i == 0) {
+					return "not prime";
+				}
+			}
+			return "prime";
+		}
+
+		public static void main (String[]args)
+		{
+
+			Scanner sc = new Scanner(System.in);
+			System.out.println("Enter a number");
+			int n = sc.nextInt();
+			System.out.println(checkPrime(n));
+
+		}
+	}
